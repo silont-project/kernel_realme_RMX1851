64cac2b69b1f6d669765f64a8d4f86d05f7943c9
Park Ju Hyung <qkrwngud825@gmail.com>
2019-06-24 21:37:21 +0900

    sysctl: promote sched_migration_cost_ns out of CONFIG_SCHED_DEBUG
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [kdrag0n: forward-ported from k4.9 to k4.14]
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -132,10 +132,11 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
+unsigned int __read_mostly sysctl_sched_migration_cost	= 500000UL;
+DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 /*
  * The exponential sliding  window over which load is averaged for shares
@@ -661,6 +662,7 @@ struct sched_entity *__pick_last_entity(struct cfs_rq *cfs_rq)
 
 	return rb_entry(last, struct sched_entity, run_node);
 }
+#endif
 
 /**************************************************************
  * Scheduling class statistics methods:
@@ -688,7 +690,6 @@ int sched_proc_update_handler(struct ctl_table *table, int write,
 
 	return 0;
 }
-#endif
 
 /*
  * delta /= w
