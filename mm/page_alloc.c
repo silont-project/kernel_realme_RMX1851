91792ef04cd2bb94f8680cc348821aae6ca428b4
kentalmanis <susukntlmanis88@gmail.com>
2021-01-04 16:51:53 +0000

    Revert "devfreq_boost: Update and expand to handle CPUBW/LLCCBW boosting"
    This reverts commit 1652dad3c3d317bb21830975fb3ff0c820e4b93f.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -3876,7 +3876,6 @@ retry:
 	/* Boost when memory is low so allocation latency doesn't get too bad */
 	cpu_input_boost_kick_max(100);
 	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
-	devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 100);
 
 	if (should_reclaim_retry(gfp_mask, order, ac, alloc_flags,
 				 did_some_progress > 0, &no_progress_loops))
